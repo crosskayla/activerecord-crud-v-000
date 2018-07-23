a76adf454e940698ff50e8a614a0cbae71bbfd52
Kayla Cross <kac288@georgetown.edu>
2018-07-23 17:18:00 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {})
   Movie.create do |m|
-    __
+    m.in_theaters = false
   end
 end
 
