150ce3fdbfccc084ec4edbf16c16f0a3fcba5263
Kayla Cross <kac288@georgetown.edu>
2018-07-23 17:07:40 +0000

    automatically backed up by learn

--- db/migrate/20180723170551_create_movies.rb
+++ db/migrate/20180723170551_create_movies.rb
@@ -1,4 +1,8 @@
 class CreateMovies < ActiveRecord::Migration
-  def change
+  def change 
+    create_table :movies do |t|
+      t.string :title 
+      t.datetime :release_date
+      t.string :director
   end
 end
